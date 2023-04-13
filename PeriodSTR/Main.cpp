7765a64e1a1c91269bf332b3ca42b9bb418f845d
Htopik <49284279+Htopik@users.noreply.github.com>
2023-04-13 23:31:19 +0400

    Нормализация комментариев

--- PeriodSTR/Main.cpp
+++ PeriodSTR/Main.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 bool IsKPeriodic(string str, int k) {
-    if (str.length() % k != 0) //≈сли длина строки не кратна k, то и говорить не о чем
+    if (str.length() % k != 0) //If the length of the string is not a multiple of k, then there is nothing to talk about
         return 0;
-    string substr = str.substr(0, k); //находим первую подстроку
+    string substr = str.substr(0, k); //finding the first substring
     for (int i = 0; i < str.length() / k; ++i) {
-        if (str.substr(i * k, k) != substr) //провер€ем, чтобы все последущие подстроки совпали с первой
-            return 0; //≈сли не совпали, строка не кратка K
+        if (str.substr(i * k, k) != substr) //check that all subsequent substrings match the first one
+            return 0; //If not matched, the string is not short K
     }
     return 1;
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    //ѕроизведЄн р€д тестов
+    //A number of tests have been performed
     cout << IsKPeriodic("abcabcabcabc", 3) << '\n';
     cout << IsKPeriodic("abcabcabcabc", 2) << '\n';
     cout << IsKPeriodic("abcdabcdabcdabcd", 4) << '\n';
